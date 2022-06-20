512b20878edb7623c9b366192034247a40c560b0
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-20 15:17:25 +0200

    Dodaj pliki projektów.

---
+++ Zadanie3.11/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+    int suma = 0, a, iloscLiczb = 0;
+    do
+    {
+        Console.WriteLine("Podaj dodatni wykladnik:");
+        a = int.Parse(Console.ReadLine());
+        suma += a;
+        iloscLiczb++;
+        Console.Clear();
+    } while (a != 0);
+    Console.WriteLine("Podałeś {0} liczb, Suma wpisanych liczb = {1}",
+        iloscLiczb,
+        suma);
+    Console.ReadLine();
+}
